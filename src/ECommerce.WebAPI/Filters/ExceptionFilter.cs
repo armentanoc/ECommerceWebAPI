2be33e278cc409267bffb6e3ea821b346e9893e6
Carolina Armentano <armentanocarolina@gmail.com>
2024-02-20 14:59:48 -0300

    refactor: logging errors caught in exception filter through logging middleware.

--- src/ECommerce.WebAPI/Filters/ExceptionFilter.cs
+++ src/ECommerce.WebAPI/Filters/ExceptionFilter.cs
@@ -5,15 +5,23 @@ namespace ECommerce.WebAPI.Filters
 {
     public class ExceptionFilter : IAsyncExceptionFilter
     {
+        private readonly ILogger<ExceptionFilter> _logger; 
+
+        public ExceptionFilter(ILogger<ExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task OnExceptionAsync(ExceptionContext context)
         {
-
             int statusCode;
 
             if (context.Exception is UnauthorizedAccessException)
                 statusCode = StatusCodes.Status401Unauthorized;
             else
                 statusCode = StatusCodes.Status500InternalServerError;
+
+            _logger.LogError(context.Exception, "Error caught in exception filter.");
 
             var objectResponse = new
             {
