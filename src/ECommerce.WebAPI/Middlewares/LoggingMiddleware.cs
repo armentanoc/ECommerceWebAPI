abba5d0ee6c5ba10e81ab3a29925f60e724bd949
armentanoc <armentanocarolina@gmail.com>
2024-02-20 12:25:29 -0300

    feat: new logging middleware.

---
+++ src/ECommerce.WebAPI/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace ECommerce.WebAPI.Middlewares
+{
+    internal class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<LoggingMiddleware> _logger;
+
+        public LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _logger.LogInformation($"Início da requisição: {context.Request.Path}");
+
+            await _next(context);
+
+            _logger.LogInformation($"Fim da requisição: {context.Request.Path}");
+        }
+    }
+}
