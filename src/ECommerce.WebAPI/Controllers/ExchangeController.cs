b597455105e327d2c1c17011abdcdd60e096e91f
Carolina Armentano <armentanocarolina@gmail.com>
2024-02-20 15:14:45 -0300

    feat: add exchange and refund base controllers.

---
+++ src/ECommerce.WebAPI/Controllers/ExchangeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerce.ViewModels;
+using ECommerce.Domain.Models;
+using ECommerce.Application.Interfaces;
+
+namespace ECommerce.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/exchanges")]
+    public class ExchangeController : Controller
+    {
+        private readonly IExchangeService _service;
+
+        public ExchangeController(IExchangeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Exchange>> GetAll()
+        {
+            var exchanges = _service.GetAll();
+            return Ok(exchanges);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<IEnumerable<Exchange>> Get([FromRoute] uint id)
+        {
+            var exchanges = _service.Get(id);
+            return Ok(exchanges);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromForm] ExchangeRequest exchangeRequest)
+        {
+            var exchange = _service.Add(exchangeRequest);
+            return Ok(exchange);
+        }
+    }
+}
---
+++ src/ECommerce.WebAPI/Controllers/RefundController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ECommerce.ViewModels;
+using ECommerce.Domain.Models;
+using ECommerce.Application.Interfaces;
+
+namespace ECommerce.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/refunds")]
+    public class RefundController : Controller
+    {
+        private readonly IRefundService _service;
+
+        public RefundController(IRefundService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Refund>> GetAll()
+        {
+            var refund = _service.GetAll();
+            return Ok(refund);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<IEnumerable<Refund>> Get([FromRoute] uint id)
+        {
+            var refund = _service.Get(id);
+            return Ok(refund);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromForm] RefundRequest request)
+        {
+            var refund = _service.Add(request);
+            return Ok(refund);
+        }
+    }
+}
